a90d9ac5c5d98ea4bef94faf2239b696244c943b
student <student@vuz.magtu.ru>
2024-01-13 11:47:24 +0500

    first 1

---
+++ product/create.php
@@ -0,0 +1,37 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+
+include_once "../config/database.php";
+include_once "../objects/product.php";
+
+$database=new Database();
+$db=$database->getConnection();
+
+$product=new Product($db);
+
+$json=file_get_contents('php://input');
+$data=json_decode($json);
+
+if(!empty($data->name) && !empty($data->description) && !empty($data->price) && !empty($data->category_id)){
+    $product->name=$data->name;
+    $product->description=$data->description;
+    $product->price=$data->price;
+    $product->category_id=$data->category_id;
+    $product->created=date('Y-m-d H:i:s');
+    
+    if($product->create()){
+        http_response_code(201);
+        echo json_encode(array("message"=>"Продукт создан"),JSON_UNESCAPED_UNICODE);
+    }
+    else{
+        http_response_code(400);
+        echo json_encode(array("message"=>"Продукт не получилось создать"),JSON_UNESCAPED_UNICODE);
+    }
+}
+else{
+    http_response_code(400);
+    echo json_encode(array("message"=>"Неврозможно создать товар данные не полные"),JSON_UNESCAPED_UNICODE);
+}
+?>
--- product/read.php
+++ product/read.php
@@ -1,6 +1,7 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET");
 
 include_once "../config/database.php";
 include_once "../objects/product.php";
@@ -23,14 +24,15 @@ if($num>0){
             "id" => $id,
             "name" => $name,
             "description" => $description,
-            "id" => $id,
-            "id" => $id,
+            "price" => $price,
+            "category_id" => $category_id,
+            "category_name" => $category_name,
         );
 
         array_push($product_arr["records"],$product_item);
-        http_response_code(200);
-        echo json_encode($product_arr);
     }
+    http_response_code(200);
+    echo json_encode($product_arr);
 }else{
     http_response_code(404);
     echo json_encode(array("message"=>"Товары не найдены"),JSON_UNESCAPED_UNICODE);
